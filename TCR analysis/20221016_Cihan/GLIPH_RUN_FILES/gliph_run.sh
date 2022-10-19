c6410c9a0dd00c54bac9b772db62d5eface4c79d
kalpanamanthiram <101271043+kalpanamanthiram@users.noreply.github.com>
2022-10-19 12:52:57 -0400

    Add files via upload

---
+++ TCR analysis/20221016_Cihan/GLIPH_RUN_FILES/gliph_run.sh
@@ -0,0 +1,9 @@
+#####CIHAN OGUZ (cihan.oguz@nih.gov). October 2022.
+#####NIAID Collaborative Bioinformatics Resource (NCBR), NIAID/NIH
+cd /working_directory
+chmod a+x irtools.osx 
+./irtools.osx -c parameter_file.txt
+
+###########
+###########
+
